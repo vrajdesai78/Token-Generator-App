5a5bc942b48779a657b82074d977d4321f5aa306
vrajdesai78 <vrajdesai78@gmail.com>
2021-05-21 16:57:49 +0530

    Animations Added

--- app/src/main/java/com/vrajdesai/myapplication/StartActivity.java
+++ app/src/main/java/com/vrajdesai/myapplication/StartActivity.java
@@ -7,12 +7,20 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.view.View;
 import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
 public class StartActivity extends AppCompatActivity {
+
+    Animation topAnim,botAnim;
+    ImageView imageView;
+    TextView tv1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +28,14 @@ public class StartActivity extends AppCompatActivity {
         setContentView(R.layout.activity_start);
 
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        topAnim = AnimationUtils.loadAnimation(this,R.anim.top_animation);
+        botAnim = AnimationUtils.loadAnimation(this,R.anim.bottom_animation);
+        tv1 = findViewById(R.id.sst);
+        imageView = findViewById(R.id.ssl);
+
+        imageView.setAnimation(topAnim);
+        tv1.setAnimation(botAnim);
 
         Handler handler = new Handler();
         handler.postDelayed(new Runnable() {
